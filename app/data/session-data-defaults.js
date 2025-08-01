e0a2f603992278836aaf683950bdd293ab5743cc
Larry Miles <larry@blackmondaydesign.com>
2025-08-01 10:29:41 +0100

    Updated the your payments screen to use read (dummy) data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,107 @@
 module.exports = {
 
+  "payments": [
+    {
+      procurment_identifier_ocid: "ocds-h6vhtk-05635a",
+      contract_number: "1",
+      buyer_ppon: "PCGB-8532-WJKN",
+      procurement_organisation_ppon: "PFRA-1094-PQVM",
+      supplier_ppon: "PZLD-6721-BRTY",
+      amount: "£3,300,500.00",
+      payment_date: "2025/9/24"
+    },
+    {
+      procurment_identifier_ocid: "gsrf-u7fekc-73941n",
+      contract_number: "1,2,3",
+      buyer_ppon: "PCGB-8532-WJKN",
+      procurement_organisation_ppon: "PXXX-1234-XXXX",
+      supplier_ppon: "PXXX-1234-XXXX",
+      amount: "£425,672.48",
+      payment_date: "2025/5/27"
+    },
+    {
+      procurment_identifier_ocid: "adgh-t5p7b6-f4j8l9",
+      contract_number: "1,2,3",
+      buyer_ppon: "PCGB-8532-WJKN",
+      procurement_organisation_ppon: "PXYQ-4389-HJNK",
+      supplier_ppon: "PDMN-9056-OLKS",
+      amount: "£425,672.48",
+      payment_date: "2025/4/24"
+    },
+    {
+      procurment_identifier_ocid: "jklm-89c0d1-g2h3j4",
+      contract_number: "1,2,3",
+      buyer_ppon: "PCGB-8532-WJKN",
+      procurement_organisation_ppon: "PVRT-3478-XZBY",
+      supplier_ppon: "PSAW-2107-CDFR",
+      amount: "£425,672.48",
+      payment_date: "2025/7/8"
+    }
+  ],
 
   
 };
+
+/*
+Dates
+
+03 May 2025
+19 June 2025
+22 April 2025
+11 June 2025
+05 May 2025
+29 April 2025
+16 May 2025
+01 June 2025
+12 May 2025
+25 June 2025
+09 April 2025
+18 May 2025
+30 June 2025
+06 April 2025
+21 May 2025
+15 June 2025
+28 April 2025
+*/
+
+/*
+PPON
+
+PJLK-5643-QWET
+POIU-7812-MNBG
+PKJM-9015-VCRF
+PNLK-1346-ZXAS
+PLKJ-8754-POIU
+PUYT-2980-NBVC
+PMNB-6537-LKJH
+PRTY-4210-ASDF
+PZXA-7890-MNBG
+POKI-5678-ERTY
+PJKL-1234-FGHJ
+PDSA-9012-QWER
+PXCV-3456-ZXCV
+*/
+
+/*
+procurment_identifier_ocid
+
+bcde-e6f7g8-h9j0k1
+pqrs-2a3b4c-5d6e7f
+uvwx-z9y8x7-w6v5u4
+fghi-k1l2m3-n4p5q6
+nopq-r7s8t9-u0v1w2
+stuv-3x4y5z-6a7b8c
+wxyz-d9e0f1-g2h3j4
+abcd-5k6l7m-8n9p0q
+efgh-r1s2t3-u4v5w6
+ijkl-7x8y9z-0a1b2c
+mnop-d3e4f5-g6h7j8
+qrst-k9l0m1-n2p3q4
+uvwx-r5s6t7-u8v9w0
+yzab-c1d2e3-f4g5h6
+cdef-j7k8l9-m0n1p2
+ghij-q3r4s5-t6u7v8
+klmn-x9y0z1-a2b3c4
+opqr-e5f6g7-h8j9k0
+*/
+
